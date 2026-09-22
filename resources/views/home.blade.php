73288f053478fdca572a3fc0cc9141e7b758dfb7
syedwasi37 <169069879+syedwasi37@users.noreply.github.com>
2026-09-22 15:21:15 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1509,7 +1509,7 @@ if ($isToolPage) {
             <?= htmlspecialchars($currentToolName, ENT_QUOTES) ?><br>
             <em>Free online tool.</em>
             <?php else: ?>
-            Hello aaalooooo<br>
+            Free Online PDF, Image &<br>
             <em>Utility Tools.</em>
             <?php endif; ?>
         </h1>
